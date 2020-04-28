c26b37c9f512e545297be91c5908e0e5e30bc3ef
Devone Austin <devone.austin@gmail.com>
2020-04-28 21:42:36 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -2,9 +2,14 @@ class Cat
   attr_accessor :owner, :mood
   attr_reader :name
 
+  @@all = []
+  
   def initialize(name)
     @name = name
     @owner = owner
     @mood = "nervous"
+    @@all < self
   end
+
+
 end
