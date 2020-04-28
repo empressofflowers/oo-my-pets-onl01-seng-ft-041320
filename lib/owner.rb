e8aedef3f62a91955fdcd7a70daea265dbe13616
Devone Austin <devone.austin@gmail.com>
2020-04-28 21:24:47 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,28 @@
 class Owner
-  # code goes here
+
+  attr_reader :name, :species
+
+  def name
+    @name = name
+  end
+
+  def species
+    @species = species
+  end
+
+  def say_species
+    puts "I am a #{species}"
+  end
+
+  def self.all
+    
+  end
+
+  def self.count
+    
+  end
+
+  def reset_all
+    
+  end
 end
