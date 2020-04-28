f6fecf537611030ba9b80012e6c81d969dfa5981
Devone Austin <devone.austin@gmail.com>
2020-04-28 22:20:26 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -43,10 +43,10 @@ class Owner
   end
 
   def walk_dogs
-    dogs.each { |dog| dog.mood = "happy"}
+    dogs.each { |dog| dog.mood = "happy" }
   end
 
   def feed_cats
-    cats.each { |cat| cat.mood = "happy"}
+    cats.each { |cat| cat.mood = "happy" }
   end
 end
